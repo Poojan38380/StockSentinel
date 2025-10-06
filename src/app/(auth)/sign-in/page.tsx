9750daf34fa66aa195895486ea552b8b073f7d43
Poojan <poojangoyani@gmail.com>
2025-10-06 22:53:04 +0530

    implemented sign in and logout functionalities.

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/ui/button';
 import InputField from '@/components/forms/InputField';
 import FooterLink from '@/components/forms/FooterLink';
 import {useRouter} from "next/navigation";
+import { signInWithEmail } from '@/lib/actions/auth.actions';
+import { toast } from 'sonner';
 
 const SignIn = () => {
     const router = useRouter()
@@ -22,13 +24,13 @@ const SignIn = () => {
 
     const onSubmit = async (data: SignInFormData) => {
         try {
-            // const result = await signInWithEmail(data);
-            // if(result.success) router.push('/');
+            const result = await signInWithEmail(data);
+            if(result.success) router.push('/');
         } catch (e) {
             console.error(e);
-            // toast.error('Sign in failed', {
-            //     description: e instanceof Error ? e.message : 'Failed to sign in.'
-            // })
+            toast.error('Sign in failed', {
+                description: e instanceof Error ? e.message : 'Failed to sign in.'
+            })
         }
     }
 

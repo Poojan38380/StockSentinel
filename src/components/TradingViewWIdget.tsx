6112addd5c37116bc4653aa59c20c513e56570d7
Poojan <poojangoyani@gmail.com>
2025-10-07 14:17:37 +0530

    trying to remove the tradingviewwidget build error

--- src/components/TradingViewWIdget.tsx
+++ src/components/TradingViewWIdget.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { cn } from '@/lib/utils';
-import React, { memo } from 'react';
+import React from 'react';
 import useTradingViewWidget from '@/hooks/useTradingViewWidget';
 
 
@@ -26,4 +26,4 @@ const TradingViewWidget = ({ title, scriptUrl, config, height, className }: Trad
     );
 }
 
-export default memo(TradingViewWidget);
+export default TradingViewWidget;

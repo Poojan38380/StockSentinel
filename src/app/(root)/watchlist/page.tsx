e446b421c0bd946fe941f42eaca60ff9268a7b65
Poojan <poojangoyani@gmail.com>
2025-10-08 03:18:03 +0530

    added the most basic version of the watchlist table.

--- src/app/(root)/watchlist/page.tsx
+++ src/app/(root)/watchlist/page.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { getWatchlistByUserId } from '@/lib/actions/watchlist.actions'
+import WatchlistTable from '@/components/Watchlist/WatchlistTable'
 
 const WatchlistPage = async () => {
-  const watchlist = await getWatchlistByUserId()
+    const watchlist = await getWatchlistByUserId()
 
-  return (
-    <div>WatchlistPage</div>
-  )
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            <section className="grid w-full gap-8 home-section">
+                <div className="md:col-span-2 xl:col-span-2">
+                    <WatchlistTable initialWatchlist={watchlist} />
+                </div>
+                
+            </section>
+
+        </div>
+    )
 }
 
 export default WatchlistPage
